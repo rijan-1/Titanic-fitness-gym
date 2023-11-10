4ad5ff95ff34aade00fc8bb8e3fc647e3a6d2d53
rijan-1 <rijanrai125@gmail.com>
2023-11-10 10:13:56 +0000

    api ninja

--- src/Options/Workoutplan.js
+++ src/Options/Workoutplan.js
@@ -1,41 +1,45 @@
-import './Workoutplan.css'
-import { useState } from 'react'
-
+import './Workoutplan.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 export function Workoutplan() {
+  const [text, setText] = useState('');
+  const [addWorkout, setAddWorkout] = useState(null);
 
-   const [text,setText] =useState('');
-   const [addList,setAddList]=useState([]);
-
-
-const HandleAddList=()=>{
-    const ListId={
-        id: addList.length===0?1:addList[addList.length -1].id +1,
-        name: text
+  const getWorkout = () => {
+    // Check if text is not empty before making the API call
+    if (text.trim() === '') {
+      console.error('Muscle parameter is empty');
+      return;
     }
-    setAddList([...addList,ListId])
+
+    axios
+      .get(`https://api.api-ninjas.com/v1/exercises?muscle=${text}`, {
+        headers: {
+          'X-Api-Key': 'nAmCzYqHGUELZqGa67Aqg==AbAEZISgNXCxq7VR',
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setAddWorkout(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching workout:', error.message);
+        // You might want to set an error state here for better user feedback
+      });
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={(event) => setText(event.target.value)} />
+      <button onClick={getWorkout}>Submit</button>
+      {/* Display workout data, assuming it's an array or an object */}
+      {addWorkout && (
+        <div>
+          <h2>Workout Details:</h2>
+          <pre>{JSON.stringify(addWorkout, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 }
-
-const Delete=(value)=>{
-    setAddList(addList.filter((plan)=>{
-        if (value===plan.id){
-            return false
-        }else{
-            return true
-        }
-    }))
-}
-
-
-
-        return (
-            <div>
-                <input type='text' onChange={(event) => { setText(event.target.value) }} />
-                <button onClick={HandleAddList}>Submit</button>
-                {addList.map((value) => {
-                    return <div><div className='listname'>{value.name}</div> <button className='buttonname'onClick={() => Delete(value.id)}>X</button></div>
-                })}
-
-            </div>
-        )
-    }
